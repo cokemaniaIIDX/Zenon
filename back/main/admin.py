6ec1c56afb835e3c4b2161107523f2cd274f24d8
coke@imade-gcp01 <sbtkzks@gmail.com>
2022-03-12 17:09:55 +0900

    モデル作成 材料、料理

--- back/main/admin.py
+++ back/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ingredient, Dish
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+  list_display = ('name', 'kind', 'price')
+  list_display_link = ('name')
+
+class DishAdmin(admin.ModelAdmin):
+  list_display = ('name', 'category')
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Dish, DishAdmin)
--- back/main/models.py
+++ back/main/models.py
@@ -1,6 +1,20 @@
+from unicodedata import category, name
 from django.db import models
 
+class Ingredient(models.Model):
+  name = models.CharField("Ingredient Name", max_length=400, blank=False)
+  kind = models.CharField("Kind", max_length=100, blank=False)
+  price = models.PositiveIntegerField("Price", blank=True, null=True)
+
+  def __str__(self):
+        return self.name
+
 class Dish(models.Model):
-  dishName = models.CharField(name="Dish Name", max_length=100)
-  picture = models.URLField(name="Picture URL")
-  
+  name = models.CharField("Dish Name", max_length=400)
+  picture = models.URLField("Picture URL", blank=True)
+  ingredient = models.ManyToManyField(Ingredient)
+  category = models.CharField("Category", max_length=100)
+  part = models.PositiveIntegerField("Part", default=1)
+
+  def __str__(self):
+        return self.name
