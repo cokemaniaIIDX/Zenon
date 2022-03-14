8b05b13219e58e808b3b9883c7be4158a43432a5
coke@imade-gcp01 <sbtkzks@gmail.com>
2022-03-14 23:41:17 +0900

    完成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import jaLocale from '@fullcalendar/core/locales/ja';
 import './App.css';
 
@@ -9,10 +10,15 @@ function App() {
   return (
     <div>
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
         initialView="dayGridMonth"
         locales={[jaLocale]}
         locale='ja'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek',
+        }}
       />
     </div>
   );
