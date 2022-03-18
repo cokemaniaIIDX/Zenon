d7a113fdcecddd6487e85435085e92e6b59b3c24
coke@imade-gcp01 <sbtkzks@gmail.com>
2022-03-18 23:05:59 +0900

    props受け渡しで曜日ヘッダ作成

--- front/src/Calendar.tsx
+++ front/src/Calendar.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-interface Props {
-  item: string;
-  children: React.ReactNode;
-}
-
 const Calendar: React.VFC = () => {
 
+  const week: string[] = ["日", "月", "火", "水", "木", "金", "土"]
   const date = new Date();
+  const dayOfWeek = date.getDay();
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
+  const month = date.getMonth() + 1;  // getMonth() は 0-11 の数字で返ってくるので +1 する
 
   const Confirm = () => {
-    console.log(year)
-    console.log(typeof(year))
+    console.log(dayOfWeek)
+    console.log(typeof(dayOfWeek))
   }
 
   return (
@@ -24,30 +21,43 @@ const Calendar: React.VFC = () => {
         <h2>{year}年 {month}月</h2>
         <table>
           <thead>
-            <ListWeek />
+            <ListWeek week={week} />
           </thead>
+          <tbody>
+            <ListDays dayOfWeek={dayOfWeek} />
+          </tbody>
         </table>
       </div>
     </div>
   )
 }
 
-const ListWeek: React.VFC = () => {
+type ListWeekProps = {
+  week: string[];
+}
 
-  const week: string[] = ["日", "月", "火", "水", "木", "金", "土"]
+const ListWeek: React.VFC<ListWeekProps> = (props) => {
+
+  const weekList = props.week
 
   return (
     <tr>
-      {week.map((item, key) =>
-        <th key={key}><Week /></th>
+      {weekList.map((item, key) =>
+        <th key={key}>{item}</th>
       )}
     </tr>
   )
 }
 
-const Week: React.VFC = () => {
+type ListDaysProps = {
+  dayOfWeek: number;
+}
+
+const ListDays: React.VFC<ListDaysProps> = (props) => {
   return (
-    <p>あ</p>
+    <tr>
+      <td>{props.dayOfWeek}</td>
+    </tr>
   )
 }
 
