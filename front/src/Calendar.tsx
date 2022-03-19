ef75737a32479997993949d5a6c28568ee9095a9
coke@imade-gcp01 <sbtkzks@gmail.com>
2022-03-19 20:35:05 +0900

    日付表示実装完了

--- front/src/Calendar.tsx
+++ front/src/Calendar.tsx
@@ -5,15 +5,29 @@ import React from "react";
 const Calendar: React.VFC = () => {
 
   const week: string[] = ["日", "月", "火", "水", "木", "金", "土"]
-  const days: string[] = ["1", "2", "3", "4", "5", "6", "7"]
+  const days: string[] = [
+    "27", "28", "1", "2", "3", "4", "5",
+    "6", "7", "8", "9", "10", "11", "12",
+    "13", "14", "15", "16", "17", "18", "19",
+    "20", "21", "22", "23", "24", "25", "26",
+    "27", "28", "29", "30", "31", "1", "2", 
+    "3", "4", "5", "6", "7", "8", "9",
+  ]
   const date = new Date();
   const dayOfWeek = date.getDay();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;  // getMonth() は 0-11 の数字で返ってくるので +1 する
 
   const Confirm = () => {
-    console.log(dayOfWeek)
-    console.log(typeof(dayOfWeek))
+    const list = week
+    const list2 = days
+    const dayList: string[][] = []
+
+    for (let i = 0; i < list2.length / list.length; i++) {
+      dayList.push(list2.slice(i*list.length,i*list.length + list.length))
+    }
+
+    console.log(dayList)
   }
 
   return (
@@ -24,10 +38,10 @@ const Calendar: React.VFC = () => {
         <h2>{year}年 {month}月</h2>
         <table>
           <thead>
-            <ListContents list={week} />
+            <ListContents weekList={week} contents={week} />
           </thead>
           <tbody>
-            <ListContents list={days} />
+            <ListContents weekList={week} contents={days} />
           </tbody>
         </table>
       </div>
@@ -39,34 +53,49 @@ const Calendar: React.VFC = () => {
 
 type ListContentsProps = {
   // 曜日リストの型宣言
-  list: string[];
+  weekList: string[];
+  contents: string[];
 }
 
 const ListContents: React.VFC<ListContentsProps> = (props) => {
 
-  const weekList = props.list
+  const listLength = props.weekList.length
+  const fullContents = props.contents
+
+  // contentsを多重配列として初期化
+  const contents: string[][] = []
+
+  for (let i = 0; i < fullContents.length / listLength; i++) {
+    contents.push(fullContents.slice(listLength*i, listLength*i + listLength))
+  }
 
   return (
-    <DisplayContents list={weekList} />
+    <>
+      {contents.map((item, key) => {
+        return (
+          <tr key={key}><DisplayContents contents={item} /></tr>
+        )
+      })}
+    </>
   )
 }
 
 // カレンダーの中身を表示するコンポーネント
 
 type DisplayContentsProps = {
-  list: string[];
+  contents: string[];
 }
 
 const DisplayContents: React.VFC<DisplayContentsProps> = (props) => {
 
-  const week = props.list
+  const contents = props.contents
 
   return (
-    <tr>
-      {week.map((item, key) =>
+    <>
+      {contents.map((item, key) =>
         <th key={key}>{item}</th>
       )}
-    </tr>
+    </>
   )
 }
 
