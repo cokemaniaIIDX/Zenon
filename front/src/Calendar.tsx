fe02491b9bab76375c68774cb735d4e787e5ec88
coke@imade-gcp01 <sbtkzks@gmail.com>
2022-03-20 23:26:43 +0900

    日付の作成と表示を分ける

--- front/src/Calendar.tsx
+++ front/src/Calendar.tsx
@@ -1,65 +1,62 @@
 import React from "react";
 
-// カレンダーを表示するコンポーネント
+// カレンダーの日付リストを作成し、表示用コンポーネントに
 
-const Calendar: React.VFC = () => {
+const CreateCalendar: React.VFC = () => {
 
-  const week: string[] = ["日", "月", "火", "水", "木", "金", "土"]
-  const days: string[] = []
+  const days: string[] = [];
   const date = new Date();
-  const dayOfWeek = date.getDay();
   const year = date.getFullYear();
-  const month = date.getMonth();  // getMonth() は 0-11 の数字で返ってくるので +1 する
-  const today = date.getDate();
+  const month = date.getMonth(); // getMonth() は 0-11 の数字で返ってくるので +1 する
   const lastDayOfThisMonth = new Date(year, month + 1, 0);
   const lastDayOfLastMonth = new Date(year, month, 0);
 
-  const Confirm = () => {
-    console.log(lastDayOfThisMonth)
-    console.log(lastDayOfLastMonth)
-    console.log("今日: " + year + "年 " + (month + 1) + "月 " + today + "日 " + week[dayOfWeek] + "曜日")
-    console.log("今月の最終日: " + year + "年 " + (month + 1) + "月 " + lastDayOfThisMonth.getDate() + "日 " + week[lastDayOfThisMonth.getDay()] + "曜日")
-    console.log("先月の最終日: " + lastDayOfLastMonth.getFullYear() + "年 " + (lastDayOfLastMonth.getMonth() + 1)+ "月 " + lastDayOfLastMonth.getDate() + "日 " + week[lastDayOfLastMonth.getDay()] + "曜日")
-    console.log("先月最終日の曜日: " + week[lastDayOfLastMonth.getDay()] + "曜日")
-    console.log("表示する先月分の日数: " + (lastDayOfLastMonth.getDay() + 1))
-    console.log("今月の日数: " + lastDayOfThisMonth.getDate())
-    console.log("表示する来月分の日数: " + (42 - (lastDayOfThisMonth.getDate() + (lastDayOfLastMonth.getDay() + 1))))
+  for (let i = 0; i < (lastDayOfLastMonth.getDay() + 1); i++) {
+    let addDay = lastDayOfLastMonth.getDate() - i;
+    days.push(addDay.toString())
   }
-
-  const CreateDays = () => {
-    for (let i = 0; i < (lastDayOfLastMonth.getDay() + 1); i++) {
-      let addDay = lastDayOfLastMonth.getDate() - i;
-      days.push(addDay.toString())
-    }
-
-    for (let i = 1; i < (lastDayOfThisMonth.getDate() + 1); i++) {
-      days.push(i.toString())
-    }
-
-    for (let i = 1; i < (41 - lastDayOfThisMonth.getDate()); i++) {
-      days.push(i.toString())
-    }
-    console.log(days)
+  for (let i = 1; i < (lastDayOfThisMonth.getDate() + 1); i++) {
+    days.push(i.toString())
+  }
+  for (let i = 1; i < (41 - lastDayOfThisMonth.getDate()); i++) {
+    days.push(i.toString())
   }
 
   return (
     <div>
-      <input type="button" value="確認！" onClick={Confirm} />
-      <div>
-        <h1>カレンダー</h1>
-        <h2>{year}年 {month + 1}月</h2>
-        <table>
-          <thead>
-            <ListContents weekList={week} contents={week} />
-          </thead>
-          <tbody>
-            <ListContents weekList={week} contents={days} />
-          </tbody>
-        </table>
-        <p>days: [{days}]</p>
-        <input type="button" value="createDays" onClick={CreateDays} />
-      </div>
+      <DisplayCalendar days={days} year={year} month={month} />
     </div>
+  )
+}
+
+// カレンダーを表示するコンポーネント
+
+type DisplayCalendarProps = {
+  days: string[];
+  year: number;
+  month: number;
+}
+
+const DisplayCalendar: React.VFC<DisplayCalendarProps> = (props) => {
+
+  const week: string[] = ["日", "月", "火", "水", "木", "金", "土"]
+  const days = props.days;
+  const year = props.year
+  const month = props.month;
+
+  return (
+    <>
+      <h1>カレンダー</h1>
+      <h2>{year}年 {month + 1}月</h2>
+      <table>
+        <thead>
+          <ListContents weekList={week} contents={week} />
+        </thead>
+        <tbody>
+          <ListContents weekList={week} contents={days} />
+        </tbody>
+      </table>
+    </>
   )
 }
 
@@ -113,4 +110,4 @@ const DisplayContents: React.VFC<DisplayContentsProps> = (props) => {
   )
 }
 
-export default Calendar;
+export default CreateCalendar;
