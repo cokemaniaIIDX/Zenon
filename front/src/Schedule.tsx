3aa130c7b136cbec4e0d85e5dfbd21638b64e351
coke@imade-home01 <sbtkzks@gmail.com>
2022-07-02 13:33:44 +0000

    カレンダー部分をコンポーネント化

--- front/src/Schedule.tsx
+++ front/src/Schedule.tsx
@@ -13,15 +13,7 @@ import {
 
 const Schedule: React.VFC = () => {
 
-  const week: string[] = ["日", "月", "火", "水", "木", "金", "土"];
-  const today = new Date();
-  const [year, setYear]   = useState<number>(today.getFullYear());
-  const [month, setMonth] = useState<number>(today.getMonth());
-  const tomorrow = new Date(year, month, today.getDate() + 1)
-  const theDayAfterTomorrow = new Date(2022, month, today.getDate() + 2)
-  const dayOfToday = week[today.getDay()];
-  const dayOfTomorror = week[tomorrow.getDay()];
-  const dayOfTheDayAfterTomorrow = week[theDayAfterTomorrow.getDay()];
+  const dateToday: Date = new Date();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -29,12 +21,8 @@ const Schedule: React.VFC = () => {
     <>
       <div>
         <h1>Schedule</h1>
-        <h2>{year} / {month + 1}</h2>
-        <div>today: {today.getDate()} ({dayOfToday})</div>
-        <div>tomorrow: {tomorrow.getDate()} ({dayOfTomorror})</div>
-        <div>the day after tomorrow: {theDayAfterTomorrow.getDate()} ({dayOfTheDayAfterTomorrow})</div>
+        <Calendar dateToday={dateToday} />
       </div>
-
       <div>
         <h2>time table</h2>
         <div>
@@ -63,12 +51,32 @@ const Schedule: React.VFC = () => {
           <h4>19:00</h4>
         </div>
       </div>
+    </>
+  )
+}
 
+type CalendarProps = {
+  dateToday: Date;
+}
+
+const Calendar: React.VFC<CalendarProps> = (props) => {
+  const weekList: string[] = ["日", "月", "火", "水", "木", "金", "土"];
+  const year = props.dateToday.getFullYear();
+  const month = props.dateToday.getMonth();
+  const dateTomo = new Date(year, month, props.dateToday.getDate() + 1)
+  const dateTDAT = new Date(year, month, props.dateToday.getDate() + 2)
+
+  return (
+    <>
+      <h2>{year} / {month + 1}</h2>
+      <div>今日 : {props.dateToday.getDate()} ({weekList[props.dateToday.getDay()]})</div>
+      <div>明日 : {dateTomo.getDate()} ({weekList[dateTomo.getDay()]})</div>
+      <div>明後日 : {dateTDAT.getDate()} ({weekList[dateTDAT.getDay()]})</div>
       <div>
         <h3>debug</h3>
         <p>
-        year  : {year}<br />
-        month : {month}<br />
+          year  : {year}<br />
+          month : {month}<br />
         </p>
       </div>
     </>
